1f195b4324708b12d90b10101b07937f6f6e4595
Gulshankartikk <210281116+Gulshankartikk@users.noreply.github.com>
2025-11-04 19:17:01 +0530

    update commit

--- chat-app/frontend/vite-project/src/main.jsx
+++ chat-app/frontend/vite-project/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from './App';
+import { Outlet } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const token = localStorage.getItem("token");
+
+const router = createBrowserRouter([
+  path: "/",
+  Element:(
+    token ? <App /> : <Login />
+  )
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+ <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
